8f7d6572afd88005058f1390a99c60b8cc8cdcaf
mertunctuncer <mertunctuncer@gmail.com>
2022-10-01 04:07:10 +0300

    Balance command impl

---
+++ src/main/kotlin/dev/peopo/orbitaldemo/commands/BalCommand.kt
@@ -0,0 +1,57 @@
+package dev.peopo.orbitaldemo.commands
+
+import dev.peopo.orbitaldemo.config.Messages
+import dev.peopo.orbitaldemo.economy.Economy
+import dev.peopo.orbitaldemo.util.*
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+
+object BalCommand : TabExecutor {
+
+	override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+		if(sender !is Player) {
+			sender.sendMessage("This command can only be used by players.")
+			return true
+		}
+
+		when(args.size) {
+			0 -> {
+				sender.checkPermission(Permissions.BALANCE_SELF)?: return true
+				val balance = Economy.getBalance(sender.uniqueId) ?: run {
+					sender.sendColorizedMessage(Messages.BALANCE_MISSING)
+					return true
+				}
+				sender.sendColorizedMessage(Messages.BALANCE_SELF) {
+					return@sendColorizedMessage it
+						.replace("{balance}", balance.toString())
+				}
+			}
+			1 -> {
+				sender.checkPermission(Permissions.BALANCE_OTHER)?: return true
+
+				val userToCheck = Bukkit.getPlayerExact(args[0])?: run { sender.sendColorizedMessage(Messages.PLAYER_NOT_ONLINE); return true }
+
+				val balance = Economy.getBalance(userToCheck.uniqueId) ?: run {
+					sender.sendColorizedMessage(Messages.BALANCE_MISSING)
+					return true
+				}
+
+				sender.sendColorizedMessage(Messages.BALANCE_OTHER) {
+					return@sendColorizedMessage it
+						.replace("{balance}", balance.toString())
+						.replace("{player}", userToCheck.name)
+				}
+			}
+			else -> sender.sendColorizedMessage(Messages.BALANCE_USAGE)
+		}
+		return true
+	}
+
+	override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? = when (args.size) {
+		1 -> Bukkit.getOnlinePlayers().map { it.name }.toMutableList()
+		else -> null
+	}
+}
