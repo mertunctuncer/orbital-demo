f6fb94d4a3e70d5a22757d78b115f2acaafdf941
mertunctuncer <mertunctuncer@gmail.com>
2022-10-01 04:40:32 +0300

    Added GNUv3 license.

--- src/main/kotlin/dev/peopo/orbitaldemo/sql/JoinLeaveListener.kt
+++ src/main/kotlin/dev/peopo/orbitaldemo/sql/JoinLeaveListener.kt
@@ -1,3 +1,13 @@
+/*
+ *      This file is part of orbital-demo
+ *
+ *     orbital-demo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *     orbital-demo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along with  orbital-demo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.peopo.orbitaldemo.sql
 
 import dev.peopo.bukkitscope.coroutine.DefaultScope
