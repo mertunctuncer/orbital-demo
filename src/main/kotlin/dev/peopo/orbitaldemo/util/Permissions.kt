3535167af53f99d04c7d07b2c1db33482911d1bb
mertunctuncer <mertunctuncer@gmail.com>
2022-10-01 04:11:17 +0300

    Perm impl

--- src/main/kotlin/dev/peopo/orbitaldemo/util/Permissions.kt
+++ src/main/kotlin/dev/peopo/orbitaldemo/util/Permissions.kt
@@ -1,4 +1,8 @@
 package dev.peopo.orbitaldemo.util
 
 enum class Permissions(val permission: String) {
+	BALANCE_SELF("economy.balance.self"),
+	BALANCE_OTHER("economy.balance.other")
 }
+
+val permissionsEnabled by lazy { config.getBoolean("use_permissions") }
