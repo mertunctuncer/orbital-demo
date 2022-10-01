f469f0ee2920b5547a17982a8e47655319c25a4f
mertunctuncer <mertunctuncer@gmail.com>
2022-10-01 04:11:11 +0300

    Message impl.

--- src/main/kotlin/dev/peopo/orbitaldemo/config/MessageConfig.kt
+++ src/main/kotlin/dev/peopo/orbitaldemo/config/MessageConfig.kt
@@ -5,7 +5,7 @@ import org.bukkit.plugin.Plugin
 
 class MessageConfig(plugin: Plugin, fileName: String) : YamlConfig(plugin, fileName) {
 
-	operator fun get(message: Messages) = this.getString(message.name.lowercase())
-	fun getMessage(message: Messages) = this.getString(message.name.lowercase())
+	operator fun get(message: Messages) = this.getString(message.name.lowercase())!!
+	fun getMessage(message: Messages) = this.getString(message.name.lowercase())!!
 	fun getColorized(message: Messages) = this.getString(message.name.lowercase())!!.colorize()
 }
--- src/main/kotlin/dev/peopo/orbitaldemo/config/Messages.kt
+++ src/main/kotlin/dev/peopo/orbitaldemo/config/Messages.kt
@@ -1,5 +1,10 @@
 package dev.peopo.orbitaldemo.config
 
 enum class Messages  {
-	NO_PERMISSION
+	NO_PERMISSION,
+	PLAYER_NOT_ONLINE,
+	BALANCE_SELF,
+	BALANCE_OTHER,
+	BALANCE_MISSING,
+	BALANCE_USAGE
 }
