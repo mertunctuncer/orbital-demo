4c2d55bc1902a2540fa54472ba55a2858b963306
mertunctuncer <mertunctuncer@gmail.com>
2022-10-01 04:07:21 +0300

    Expanded command commons

--- src/main/kotlin/dev/peopo/orbitaldemo/util/CommandCommons.kt
+++ src/main/kotlin/dev/peopo/orbitaldemo/util/CommandCommons.kt
@@ -4,7 +4,15 @@ import dev.peopo.orbitaldemo.config.Messages
 import org.bukkit.entity.Player
 
 fun Player.checkPermission(permission: Permissions) : Boolean?{
-	if(!player?.hasPermission(permission.permission)!!) {
+	if(!permissionsEnabled && !player?.hasPermission(permission.permission)!!) {
+		player?.sendMessage(messages.getColorized(Messages.NO_PERMISSION))
+		return null
+	}
+	return true
+}
+
+fun Player.checkOP() : Boolean? {
+	if(!this.isOp) {
 		player?.sendMessage(messages.getColorized(Messages.NO_PERMISSION))
 		return null
 	}
@@ -13,5 +21,8 @@ fun Player.checkPermission(permission: Permissions) : Boolean?{
 
 fun Player.sendColorizedMessage(message: Messages) = this.sendMessage(messages.getColorized(message))
 
-
+fun Player.sendColorizedMessage(message: Messages, transform: (message: String) -> String) : String {
+	val transformedMessage = transform(messages.getMessage(message))
+	return transformedMessage.colorize()
+}
 
